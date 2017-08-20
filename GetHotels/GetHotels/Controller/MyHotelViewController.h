06f7b797fc622c0be4ceb981c51465b37034cc91
guuujie <1416327411@qq.com>
2017-08-20 14:11:54 +0800

    hotel

---
+++ GetHotels/GetHotels/Controller/MyHotelViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyHotelViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/20.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyHotelViewController : UIViewController
+
+@end
