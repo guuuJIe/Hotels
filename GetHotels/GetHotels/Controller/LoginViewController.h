dd41ef922848a9863e66be9f5ecfb614ea15cbc0
xieweiming <3052467868@qq.com>
2017-08-21 21:15:42 +0800

    登录界面部分代码

---
+++ GetHotels/GetHotels/Controller/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  GetHotels
+//
+//  Created by mac on 2017/8/21.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
