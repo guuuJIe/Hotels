493085ef5ce1c6e2880eec5f1bcea97d61cb7689
xieweiming <3052467868@qq.com>
2017-08-22 17:55:14 +0800

    小修改

---
+++ GetHotels/GetHotels/Controller/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  GetHotels
+//
+//  Created by mac on 2017/8/22.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
