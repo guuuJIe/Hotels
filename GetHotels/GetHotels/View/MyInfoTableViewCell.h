088799517e1c06d309d58869b9cbf3b67df383d0
htw <1490018566@qq.com>
2017-08-22 19:46:03 +0800

    no message

---
+++ GetHotels/GetHotels/View/MyInfoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyInfoTableViewCell.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/22.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyInfoTableViewCell : UITableViewCell
+
+@end
