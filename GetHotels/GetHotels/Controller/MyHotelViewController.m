504af19c0d0767cd24f45699ffc23c36b95eb907
guuujie <1416327411@qq.com>
2017-08-23 18:52:03 +0800

    no message

--- GetHotels/GetHotels/Controller/MyHotelViewController.m
+++ GetHotels/GetHotels/Controller/MyHotelViewController.m
@@ -69,12 +69,14 @@
     //设置导航栏的背景颜色
     //self.navigationController.navigationBar.backgroundColor = [UIColor whiteColor];
     [self.navigationController.navigationBar setBarTintColor:UIColorFromRGB(23, 115, 232)];
+    //设置导航条标题颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor] };
     //实例化一个button，类型为UIButtonTypeSystem
     UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     //设置位置大小
     leftBtn.frame = CGRectMake(0, 0, 20, 20);
     //设置背景图片为返回图片
-    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回"] forState:UIControlStateNormal];
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回3"] forState:UIControlStateNormal];
     //给按钮添加事件
     [leftBtn addTarget:self action:@selector(leftButtonAction:) forControlEvents: UIControlEventTouchUpInside];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftBtn];
@@ -82,7 +84,7 @@
 
 //自定义的返回按钮的事件
 - (void)leftButtonAction:(UIButton *)sender {
-    [self.navigationController popViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
     
 }
 
