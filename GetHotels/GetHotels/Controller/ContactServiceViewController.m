a8c2a9db6c8144040f4aa869c6fb70adde531dfc
xieweiming <3052467868@qq.com>
2017-08-27 19:38:45 +0800

    service

---
+++ GetHotels/GetHotels/Controller/ContactServiceViewController.m
@@ -0,0 +1,112 @@
+//
+//  ContactServiceViewController.m
+//  GetHotels
+//
+//  Created by mac on 2017/8/27.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import "ContactServiceViewController.h"
+
+@interface ContactServiceViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *contactServiceTableView;
+@property (strong, nonatomic) NSArray *arr;
+
+@end
+
+@implementation ContactServiceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    //不可变数组的初始化
+    _arr = @[@{@"title":@"酒店官方网站",@"content":@"https://gethotels.fisheep.com.cn"},@{@"title":@"酒店地址",@"content":@"江苏省无锡市滨湖区雪浪街道99号"},@{@"title":@"客服QQ",@"content":@"2200000123"},@{@"title":@"客服电话",@"content":@"0000-1234567"}];
+    
+    //去掉tableview底部多余的线
+     _contactServiceTableView.tableFooterView = [UIView new];
+    
+    //把状态栏变成白色
+    [[UIApplication sharedApplication]setStatusBarStyle:UIStatusBarStyleLightContent animated:NO];
+    
+    //调用设置导航样式
+    [self setNavigationItem];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+//设置导航样式
+- (void)setNavigationItem {
+    //设置导航栏标题
+    self.navigationItem.title = @"联系客服";
+    //设置导航条的标题颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+    //设置导航栏的背景颜色
+    [self.navigationController.navigationBar setBarTintColor:UIColorFromRGB(23, 115, 232)];
+    //self.navigationController.navigationBar.backgroundColor = [UIColor blueColor];
+    //实例化一个button，类型为UIButtonTypeSystem
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    //设置位置大小
+    leftBtn.frame = CGRectMake(0, 0, 20, 20);
+    //设置导航条是否隐藏
+    self.navigationController.navigationBar.hidden = NO;
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent = YES;
+    //设置背景图片为返回图片
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回3"] forState:UIControlStateNormal];
+    //给按钮添加事件
+    [leftBtn addTarget:self action:@selector(leftButtonAction:) forControlEvents: UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftBtn];
+}
+
+
+//自定义的返回按钮的事件
+- (void)leftButtonAction:(UIButton *)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+//每组多少行
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _arr.count;
+}
+
+//细胞长什么样
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"contactServiceCell" forIndexPath:indexPath];
+    NSDictionary *dict = _arr[indexPath.row];
+    cell.textLabel.text = dict[@"title"];
+    cell.detailTextLabel.text = dict[@"content"];
+    return cell;
+}
+
+//设置每行高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 50.f;
+}
+
+//细胞选中后调用
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //取消选中
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+@end
