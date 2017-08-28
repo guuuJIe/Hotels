0c4d7c1609f573be1248eba22aba3cc0780c9bfc
xieweiming <3052467868@qq.com>
2017-08-28 19:53:43 +0800

    支付代码

--- GetHotels/GetHotels/Controller/PurchaseTableViewController.m
+++ GetHotels/GetHotels/Controller/PurchaseTableViewController.m
@@ -9,9 +9,11 @@
 #import "PurchaseTableViewController.h"
 
 @interface PurchaseTableViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *hotelNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (strong, nonatomic) NSArray *arr;
 
 @end
 
@@ -20,11 +22,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //把状态栏变成白色
+    [[UIApplication sharedApplication]setStatusBarStyle:UIStatusBarStyleLightContent animated:NO];
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    //调用设置导航样式
+    [self naviConfig];
+    [self uiLayout];
+    [self dataInitilize];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,27 +42,83 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+//设置导航样式
+-(void)naviConfig {
+    //设置导航条的标题文字
+    self.navigationItem.title = @"支付";
+    //为导航条右上角创建一个按钮
+    UIBarButtonItem *right = [[UIBarButtonItem alloc] initWithTitle:@"支付" style:UIBarButtonItemStylePlain target:self action:@selector(payAction)];
+    self.navigationItem.rightBarButtonItem = right;
+    
+}
+
+//专门做界面
+- (void)uiLayout {
+    _hotelNameLabel.text = _hotelModel.hotel_name;
+    _dateLabel.text = [NSString stringWithFormat:@"%@--%@",_hotelModel.start_time,_hotelModel.out_time];
+    _priceLabel.text = [NSString stringWithFormat:@"%@元",_hotelModel.now_price];
+    self.tableView.tableFooterView = [UIView new];
+    //将表格视图设置为"编辑中"
+    self.tableView.editing = YES;
+    
+    //创建细胞的行号和组号(第一行，第一组)
+    NSIndexPath *indexpath = [NSIndexPath indexPathForRow:0 inSection:0];
+    //用代码来选中表格视图中的某个细胞(默认选中某个细胞)
+    [self.tableView selectRowAtIndexPath:indexpath animated:YES scrollPosition:UITableViewScrollPositionNone];
+    
+}
+
+- (void)dataInitilize {
+    _arr = @[@"支付宝支付",@"微信支付",@"银联支付"];
+}
+
+
 #pragma mark - Table view data source
 
+//设置组的标题
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"支付方式";
+}
+
+//一共多少组
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
+//每组多少行
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    
+    return _arr.count;;
 }
 
-/*
+//设置每一组中每一行的细胞长什么样
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"payCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = _arr[indexPath.row];
     
     return cell;
 }
-*/
+
+//设置每行细胞的高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50.f;
+    
+}
+
+//细胞选中后调用
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //遍历表格视图中所有选中的细胞
+    for (NSIndexPath *eachIP in tableView.indexPathsForSelectedRows) {
+        //当选中的细胞不是当前正在按的这个细胞的情况下
+        if (eachIP != indexPath) {
+            //将细胞从选中状态改为不选中状态
+            [tableView deselectRowAtIndexPath:eachIP animated:YES];
+        }
+    }
+}
+
 
 /*
 // Override to support conditional editing of the table view.
