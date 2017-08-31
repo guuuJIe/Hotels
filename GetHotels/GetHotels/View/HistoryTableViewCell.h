5117c557fd6132dc8fea794eb72f4fae1d64f9ee
linyun <571451221@qq.com>
2017-08-31 18:38:50 +0800

    我的发布

---
+++ GetHotels/GetHotels/View/HistoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewCell.h
+//  GetHotels
+//
+//  Created by admin on 2017/8/31.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewCell : UITableViewCell
+
+@end
