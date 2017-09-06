38d0122424a9e9209869ec5133648bbacb408af4
htw <1490018566@qq.com>
2017-09-06 14:24:45 +0800

    no message

---
+++ GetHotels/GetHotels/Utility/UIView+Frame.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Frame.h
+//  GetHotels
+//
+//  Created by admin on 2017/9/6.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGSize size;
+
+@end
---
+++ GetHotels/UIView+Frame.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Frame.m
+//  
+//
+//  Created by admin on 2017/9/6.
+//
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+@end
