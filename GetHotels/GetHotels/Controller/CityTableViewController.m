c18c93d3f6a053f97a347d6b704675142ce9849b
gao2550484979 <gao2550484979@gmail.com>
2017-09-12 10:41:11 +0800

    879

--- GetHotels/GetHotels/Controller/CityTableViewController.m
+++ GetHotels/GetHotels/Controller/CityTableViewController.m
@@ -161,9 +161,16 @@
     NSString *key = _keys[indexPath.section];
     NSArray *sectionCities = _cities[key];
     NSDictionary *city = sectionCities[indexPath.row];
-    [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:[NSNotification notificationWithName:@"ResetCity" object:city[@"name"]] waitUntilDone:YES];
-    [self dismissViewControllerAnimated:YES completion:nil];
+    if(_tag == 1){
+        [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:[NSNotification notificationWithName:@"ResetCity" object:city[@"name"]] waitUntilDone:YES];
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    else{
+        [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:[NSNotification notificationWithName:@"Resetcity" object:city[@"name"]] waitUntilDone:YES];
+        [self.navigationController popViewControllerAnimated:YES];
+    }
 }
+
 
 //设置右侧快捷键栏
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
